bee0e2f0f17ca1ff176e16faf690e016c2ef7b31
Remeklago <remigiusz.lagodzki@gmail.com>
2022-10-28 07:58:41 +0200

    add MainActivity3 with automaticly creating tables

--- app/src/main/java/v1/projectTech/MainActivity3.java
+++ app/src/main/java/v1/projectTech/MainActivity3.java
@@ -3,6 +3,12 @@ package v1.projectTech;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +16,46 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        createOneRow();
+
+
+    }
+
+    public void createLayouts() {
+        int tmpnumberOfRows = MainActivity2.temp3.size();
+        String[] numberOfLayouts = new String[5];
+        for (int i = 0; i < 5; i++) {
+
+            LinearLayout linearLayout3_1_in = findViewById(R.id.layout4_1);
+
+        }
+     }
+
+    public void createOneRow() {
+//        int tmpNumberOfColumns = InformacjaOLokalizacji.numberOfColumns;
+
+        LinearLayout linearLayout3_1_in = findViewById(R.id.layout3_1);
+        LinearLayout linearLayout4_1_in = findViewById(R.id.layout4_1);//find the linear layout
+        linearLayout4_1_in.removeAllViews();   //add this too
+
+            for (int i = 0; i < 5; i++) {                                                        //looping to create 5 textviews
+                TextView textView = new TextView(this);                                    //dynamically create textview
+                textView.setLayoutParams(new LinearLayout.LayoutParams(             //select linearlayoutparam- set the width & height
+                        ViewGroup.LayoutParams.WRAP_CONTENT, 40));
+                textView.setGravity(Gravity.FILL_VERTICAL);                       //set the gravity
+                textView.setText("Textview: " + i);                                    //adding text
+                linearLayout4_1_in.addView(textView);           //inflating :)
+            }
+    }
+
+    public void createTableFromRows() {
+        int tmpnumberOfRows = MainActivity2.temp3.size();
+
+        for (int i = 0; i < 10; i++) {
+            createLayouts();
+            createOneRow();
+        }
+
     }
 }
