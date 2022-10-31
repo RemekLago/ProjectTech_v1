ed07f24db23738902ccf901cb0f38b3fcba0af52
Remeklago <remigiusz.lagodzki@gmail.com>
2022-10-31 12:46:38 +0100

    add MainActivity4 with recyclerView

--- app/src/main/java/v1/projectTech/MainActivity4.java
+++ app/src/main/java/v1/projectTech/MainActivity4.java
@@ -11,11 +11,11 @@ import java.util.ArrayList;
 
 public class MainActivity4 extends AppCompatActivity {
 
-//    DBTest3 dbtest3 = new DBTest3();
-//    ArrayList<ArrayList> temp3 = dbtest3.doInBackground();
-//    int numberOfRows = temp3.size();
-//    int numberOfColumns = InformacjaOLokalizacji.numberOfColumns;
-//    ArrayList<ArrayList> FinalListWithColumnsData = temp3;
+    DBTest3 dbtest3 = new DBTest3();
+    ArrayList<ArrayList> temp3 = dbtest3.doInBackground();
+    int numberOfRows = temp3.size();
+    int numberOfColumns = InformacjaOLokalizacji.numberOfColumns;
+    ArrayList<ArrayList> FinalListWithColumnsData = temp3;
     ArrayList<ArrayList> data1;
 
     @Override
@@ -23,41 +23,41 @@ public class MainActivity4 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
-        data1 = new ArrayList<>();
-//        data1 = FinalListWithColumnsData;
+//        data1 = new ArrayList<>();
+        data1 = FinalListWithColumnsData;
 
-        ArrayList<String> row1 = new ArrayList<>();
-        row1.add(0, "Ewa1");
-        row1.add(1, "Ewa2");
-        row1.add(2, "Ewa3");
-        row1.add(3, "Ewa4");
-        row1.add(4, "Ewa5");
-
-        ArrayList<String> row2 = new ArrayList<>();
-        row2.add(0, "Adam1");
-        row2.add(1, "Adam2");
-        row2.add(2, "Adam3");
-        row2.add(3, "Adam4");
-        row2.add(4, "Adam5");
-
-        ArrayList<String> row3 = new ArrayList<>();
-        row3.add(0, "Iza1");
-        row3.add(1, "Iza2");
-        row3.add(2, "Iza3");
-        row3.add(3, "Iza4");
-        row3.add(4, "Iza5");
-
-        ArrayList<String> row4 = new ArrayList<>();
-        row4.add(0, "Jan1");
-        row4.add(1, "Jan2");
-        row4.add(2, "Jan3");
-        row4.add(3, "Jan4");
-        row4.add(4, "Jan5");
-
-        data1.add(0, row1);
-        data1.add(1, row2);
-        data1.add(2, row3);
-        data1.add(3, row4);
+//        ArrayList<String> row1 = new ArrayList<>();
+//        row1.add(0, "Ewa1");
+//        row1.add(1, "Ewa2");
+//        row1.add(2, "Ewa3");
+//        row1.add(3, "Ewa4");
+//        row1.add(4, "Ewa5");
+//
+//        ArrayList<String> row2 = new ArrayList<>();
+//        row2.add(0, "Adam1");
+//        row2.add(1, "Adam2");
+//        row2.add(2, "Adam3");
+//        row2.add(3, "Adam4");
+//        row2.add(4, "Adam5");
+//
+//        ArrayList<String> row3 = new ArrayList<>();
+//        row3.add(0, "Iza1");
+//        row3.add(1, "Iza2");
+//        row3.add(2, "Iza3");
+//        row3.add(3, "Iza4");
+//        row3.add(4, "Iza5");
+//
+//        ArrayList<String> row4 = new ArrayList<>();
+//        row4.add(0, "Jan1");
+//        row4.add(1, "Jan2");
+//        row4.add(2, "Jan3");
+//        row4.add(3, "Jan4");
+//        row4.add(4, "Jan5");
+//
+//        data1.add(0, row1);
+//        data1.add(1, row2);
+//        data1.add(2, row3);
+//        data1.add(3, row4);
 
         RecyclerView recyclerView1 = findViewById(R.id.recyclerView1);
 
